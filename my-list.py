33b2b8911cad7d89b885872624e9cb6173920296
Garissa-Codes <danielndeto222@gmail.com>
2024-12-02 23:37:16 +0300

    Initial commit: Add my-list.py

---
+++ my-list.py
@@ -0,0 +1,24 @@
+# Creating an empty list
+my_list = []
+
+# Appending the elements 10, 20, 30, 40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Inserting 15 at the second position
+my_list.insert(1, 15)
+
+# Extending my_list with [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+# SRemoving the last element from my_list
+my_list.pop()
+
+# Sorting my_list in ascending order
+my_list.sort()
+
+# Find and print the index of the value 30
+index_30 = my_list.index(30)
+print(index_30)
